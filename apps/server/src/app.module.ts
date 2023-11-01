58d4a1d5f8227aaa98f13d28822723c7e80e61ee
Nawodya Ishan <nawodyain@gmail.com>
2023-11-01 12:42:13 +0530

    feat: add next app

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,7 +4,7 @@ import {AppService} from './app.service';
 import {ConfigModule} from "@nestjs/config";
 
 @Module({
-    imports: [ConfigModule],
+    imports: [ConfigModule.forRoot()],
     controllers: [AppController],
     providers: [AppService],
 })
