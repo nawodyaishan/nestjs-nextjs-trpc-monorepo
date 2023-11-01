7fefa94ddaaae3fd9c67ed147e294a8e924eef5a
Nawodya Ishan <nawodyain@gmail.com>
2023-11-01 14:29:55 +0530

    feat: add client api call

--- apps/web/app/trpc.ts
+++ apps/web/app/trpc.ts
@@ -4,7 +4,7 @@ import { AppRouter } from "@server/trpc/trpc.router";
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: " http://localhost:4000/trpc",
+      url: " http://localhost:4000/trpc", // TODO: Use env
     }),
   ],
 });
