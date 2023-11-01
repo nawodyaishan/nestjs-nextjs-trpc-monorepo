7079d5e3be3f3da35c50ff7be64cc8bf4e1356d1
Nawodya Ishan <nawodyain@gmail.com>
2023-11-01 12:37:33 +0530

    feat: add nest config module

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,10 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 4000);
 }
 
-bootstrap();
+bootstrap().then(_r => ("Nest Bootstrap Function"));
